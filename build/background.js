07654a148408bd3eae55734e27c93dcf7b0e6ee0
Jeroen Van den Berghe <vandenberghe.jeroen@gmail.com>
2020-11-12 23:19:50 +0100

    only execute on bitbucket PR pages

--- build/background.js
+++ build/background.js
@@ -1,3 +1,3 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function() {
-  chrome.tabs.executeScript(null,{file:"bitbucket-pr-file-review.js"});
-});
+chrome.webNavigation.onHistoryStateUpdated.addListener(function () {
+  chrome.tabs.executeScript(null, { file: 'bitbucket-pr-file-review.js' });
+}, {url: [{hostEquals: 'bitbucket.org', pathContains : 'pull-requests'}]});
