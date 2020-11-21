75bd9a810f54a67e5837448b5e7da03b4b12d495
Jeroen Van den Berghe <vandenberghe.jeroen@gmail.com>
2020-11-21 16:29:14 +0100

    inject style when spa navigation happens

--- build/background.js
+++ build/background.js
@@ -1,3 +1,4 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function () {
   chrome.tabs.executeScript(null, { file: 'bb-pr-markfilesreviewed.js' });
+  chrome.tabs.insertCSS(null, { file: 'styles.css' });
 }, {url: [{hostEquals: 'bitbucket.org', pathContains : 'pull-requests'}]});
