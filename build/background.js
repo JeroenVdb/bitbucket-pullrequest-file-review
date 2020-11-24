07f010754a2a2abb4d12436ee65704e1c8ed9b8c
Jeroen Van den Berghe <vandenberghe.jeroen@gmail.com>
2020-11-24 16:25:13 +0100

    prettier

--- build/background.js
+++ build/background.js
@@ -1,4 +1,7 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function () {
-  chrome.tabs.executeScript(null, { file: 'bb-pr-markfilesreviewed.js' });
-  chrome.tabs.insertCSS(null, { file: 'styles.css' });
-}, {url: [{hostEquals: 'bitbucket.org', pathContains : 'pull-requests'}]});
+chrome.webNavigation.onHistoryStateUpdated.addListener(
+	function () {
+		chrome.tabs.executeScript(null, { file: 'bb-pr-markfilesreviewed.js' });
+		chrome.tabs.insertCSS(null, { file: 'styles.css' });
+	},
+	{ url: [{ hostEquals: 'bitbucket.org', pathContains: 'pull-requests' }] }
+);
